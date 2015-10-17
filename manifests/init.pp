7f766b28d8f3f86726e3ac7263f9276eca565f03
Ugo Paternostro <ugo.git@paternostro.org>
2015-10-17 23:37:22 +0200

    Adding support for generic features

--- manifests/init.pp
+++ manifests/init.pp
@@ -52,6 +52,9 @@
 # [*relay_domains*]
 #   List of domains to relay email for. Example: ['example.com','example2.co.uk']
 #
+# [*features*]
+#   List of features to enable. Example: [ 'relay_entire_domain' ]
+#
 # === Examples
 #
 #  class { sendmail:
@@ -80,7 +83,8 @@ class sendmail (
   $generics_table           = undef,
   $listen_ip                = '127.0.0.1',
   $is_relay                 = undef,
-  $relay_domains            = $sendmail::params::relay_domains
+  $relay_domains            = $sendmail::params::relay_domains,
+  $features                 = []
 ) inherits sendmail::params {
     package { $sendmail::params::sendmail_pkgs: ensure => latest }
 
